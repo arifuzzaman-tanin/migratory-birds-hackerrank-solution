72280e27d1303ffabb33cf53143fada5cf262805
arifuzzaman-tanin <arifuzzaman.tanin@outlook.com>
2022-08-07 01:54:44 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+function convertArrayToObject(arr) {
+    let map = {};
+    for (let i = 0; i < arr.length; i++) {
+        if (!map[arr[i]]) {
+            map[arr[i]] = 1;
+        } else {
+            map[arr[i]] = map[arr[i]] + 1;
+        }
+    }
+    return map;
+}
+
+function migratoryBirds(arr) {
+    arr = arr.sort();
+    let map = convertArrayToObject(arr);
+    let minElement = arr[0];
+    let maxElement = arr[arr.length - 1];
+
+    let maxValue = 0;
+    let result = 0;
+    for (let x in map) {
+        if (map[x] > maxValue) {
+            maxValue = map[x];
+            result = x;
+        }
+    }
+
+    return result;
+}
+
+console.log(migratoryBirds([1, 4, 4, 4, 5, 3]));
